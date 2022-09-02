320022cb787a0f8cfda7b2daa317aa74a1ec24de
Jenny Nguyen <37790333+jknguyen22@users.noreply.github.com>
2022-09-02 16:38:48 -0500

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
+// index.js is the first file to execute
+
+// we're importing react-dom from package.json
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// createRoot creates the user interface for the user, leads us to the public/index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// <App /> is a component using jsx syntax
+// it will render in the place of root's div
+root.render(<App />);
