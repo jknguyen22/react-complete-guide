79ae5dd8d6d014f341e86f7d9530413f4f1bae4f
Jenny Nguyen <37790333+jknguyen22@users.noreply.github.com>
2022-09-04 14:36:25 -0500

    add ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
 
-//must export App in order to inport App component in index.js
+//must export App in order to import App component in index.js
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+// common convention to name new component file like this
+// a component in react is JUST a javascript function
+
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
+  // may only have one root element per JSX code snippet
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
